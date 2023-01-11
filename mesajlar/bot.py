1d9b86a3adfd8a9ae36e0af929c929516b640488
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-01-11 13:20:00 +0400

    Update bot.py

--- mesajlar/bot.py
+++ mesajlar/bot.py
@@ -1,14 +1,7 @@
-# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
-# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
-# Öz adına çıxaran peysərdi
-# Bu yazıları silmədən işlədin
-
-# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
-# t.me/aykhan_s | insta: aykhan026 | 
-# GitHub: aykhan026
-
 
 
 yeni_user = (
-"Əziz Və Dəyərli user Aramıza Xoş Gəldin 😍,\nSəni Öz Aramızda Görməkdən Məmnunuq 😀.\nİnanırıqkı, Öz Gəlişinlə Bizim Qrupa Tam Bir Fərqlilik Qatacaqsan.🤩\nYaxşıkı Gəldin🥰"
+"Xoş gəlin Əzizim 🥰",
+"Gözəl Qrupumuza Xoş Gəldin 🤩",
+"Çoxdandı Səni Gözləyirəm Haralarda İdin? 😀",
 )
