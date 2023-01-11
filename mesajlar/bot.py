e8e54a83fd6406783db842508c947607795ea0d6
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-01-11 09:35:41 +0400

    Update bot.py

--- mesajlar/bot.py
+++ mesajlar/bot.py
@@ -7,16 +7,8 @@
 # t.me/aykhan_s | insta: aykhan026 | 
 # GitHub: aykhan026
 
+
+
 yeni_user = (
-"Xoş Gəldin Balam 😍",
-"Səni gözləməydən gözüm qaralıbe gəl çıxdaa",
-"Xoşsann Ürəyimm ❤️",
-)
-
-start = (
-"👋 Salam Xoş Gördük.\n✅ Mən XAOS Federasiyasının Rəsmi Söhbət Botuyam\nℹ Bot Barədə tam Məlumat Almaq Üçün /Xaosinfo  Yazın",
-)
-
-info = (
-"☑ Mən XAOS Federasiyasının Rəsmi Söhbət Botuyam\n\nℹ Botumuz Haqda Qısa Məlumat\n⚡ 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋  Sıradan Söhbət Və Avto Cavablayıcı Botudur. Bu Botun Əsas Özəlliyi Qruplarda Mesaj Sayın Çoxaltmaq , Vaxdınızı Əyləncəli Keçirmək Dir. 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋 u qruplarınlza Alaraq Vaxdınızı Əyləncəli Və Səmərəli Keçirə Bilərsiz.\n\n\n\n➕[❰ QRUPA ƏLAVƏ ET ❱➕](https://t.me/XAOS_Chatbot?startgroup=true)\n\n\n\n**👨‍💻 OWNER 🇦🇿** [XAOS KÖLGƏ](https://t.me/sesizKOLGE)",
+"Əziz Və Dəyərli user Aramıza Xoş Gəldin 😍,\nSəni Öz Aramızda Görməkdən Məmnunuq 😀.\nİnanırıqkı, Öz Gəlişinlə Bizim Qrupa Tam Bir Fərqlilik Qatacaqsan.🤩\nYaxşıkı Gəldin🥰"
 )
