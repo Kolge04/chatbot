d9f3c7eda69066f1c8210248ec5573f737aa81c5
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-02-03 23:18:50 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -225,8 +225,8 @@ async def cancel(event):
 
 seherler = "Ağcabədi Ağdam Ağdaş Ağdərə Ağıstafa Ağsu Astara Babək Bakı Balakən Beyləqan Bərdə Biləsuvar Cəbrayıl Cəlilabad Culfa Daşkəsən Dəliməmmədli Xocalı Füzuli Gədəbəy Gəncə Goranboy Göyçay Göygöl Göytəpə Hacıqabul Horadiz Xaçmaz Xankəndi Xocalı Xocavənd Xırdalan Xızı Xudat İmişli İsmayıllı Kəlbəcər Kürdəmir Qax Qazax Qəbələ Qobustan Qovlar Quba Qubadlı Qusar Laçın Lerik Lənkəran Liman Masallı Naftalan Naxçıvan Neftçala Oğuz Ordubad Saatlı Sabirabad Salyan Samux Siyəzən Sumqayıt Şuşa Şabran Şahbuz Şamaxı Şəki Şəmkir Şərur Şirvan Tərtər Tovuz Ucar Yardımlı Yevlax Zaqatala Zəngilan Zərdab󠁧󠁢󠁷󠁬󠁳󠁿󠁧󠁢󠁷󠁬󠁳󠁿".split(" ")
 
-
-@client.on(events.NewMessage(pattern="^/rtag ?(.*)"))
+@client.on(events.NewMessage(pattern=".rtag"))
+@client.on(events.NewMessage(pattern="/rtag"))
 async def mentionall(event):
   global anlik_calisan
   if event.is_private:
